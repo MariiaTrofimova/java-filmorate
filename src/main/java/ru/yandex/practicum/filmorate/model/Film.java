89c11897e5e7e19317bf27ff360042f7cdde5af9
MariiaTrofimova <mariiatr@yandex.ru>
2023-03-30 19:48:32 +0300

    style: убрать лишний импорт

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -3,10 +3,12 @@ package ru.yandex.practicum.filmorate.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Singular;
 import ru.yandex.practicum.filmorate.validation.ReleaseConstraint;
 
-import javax.validation.constraints.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
